16b7a2212ea921b9801eac1909d40ae1402a6345
Ira Horecka <ira89@icloud.com>
2021-05-13 01:04:39 -0700

    add python/

---
+++ python/rename_images.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+
+def parse_filenames(path):
+    def parse_folder(path):
+        # Get current directory name
+        dir_name = path.split("/")[-1] if path.split("/")[-1] != "" else path.split("/")[-2]
+        file_count = 1
+        for file in os.listdir(path):
+            file = os.path.join(path, file)
+            if os.path.isfile(file):
+                extension = file.split(".")[-1]
+                os.rename(
+                    file,
+                    os.path.join(path, f"{dir_name}_{f'{file_count:02}.{extension}'}"),
+                )
+                file_count += 1
+
+    directories = [
+        os.path.join(path, dir_) for dir_ in os.listdir() if os.path.isdir(os.path.join(path, dir_))
+    ]
+    list(map(parse_folder, directories))
+
+
+if __name__ == "__main__":
+    arg = sys.argv[1]
+    parse_filenames(arg)
